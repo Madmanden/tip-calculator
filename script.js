358c0fd1afade0ac196daf70d443b1bdda210926
Christian Holm <chr100@gmail.com>
2022-07-13 20:23:05 +0200

    Create script.js
    Add JS logic

---
+++ script.js
@@ -0,0 +1,98 @@
+//// Variables init
+let billAmount = null;
+let totalAmount = null;
+let tipAmount = null;
+let tipDecimal = null;
+let tipPercent = null;
+let numberOfPeople = null;
+
+// Bill input
+const bill = document.querySelector(".bill-area__amount-input");
+
+bill.addEventListener("change", () => {
+  billAmount = parseInt(bill.value);
+  runMath();
+});
+
+// Tip amount buttons
+const tipButtons = document.querySelectorAll(".tip-amount");
+
+function removeAllActive() {
+  tipButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+}
+
+tipButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (button.classList.contains("active")) {
+      button.classList.remove("active");
+      tipPercent = null;
+    } else {
+      removeAllActive();
+      button.classList.add("active");
+      tipPercent = parseInt(button.dataset.tipvalue);
+      runMath();
+    }
+  });
+});
+
+// Custom tip input
+const customInput = document.querySelector(".custom-tip");
+
+customInput.addEventListener("change", () => {
+  tipPercent = parseInt(customInput.value);
+  runMath();
+});
+
+// Number of people
+const numberOfPeopleEl = document.querySelector(".number-people-area__input");
+
+numberOfPeopleEl.addEventListener("change", (event) => {
+  numberOfPeople = parseInt(numberOfPeopleEl.value);
+  runMath();
+});
+
+// Reset button
+const reset = document.querySelector(".btn-reset");
+reset.addEventListener("click", () => {
+  billAmount = null;
+  totalAmount = null;
+  tipAmount = null;
+  tipDecimal = null;
+  tipPercent = null;
+  numberOfPeople = null;
+  tipPerPerson = null;
+  totalPerPerson = null;
+  bill.value = "";
+  customInput.value = "";
+  numberOfPeopleEl.value = "";
+  tipPerPersonEl.innerText = "";
+  totalPerPersonEl.innerText = "";
+});
+
+// UI elements to update tip/total amount per person
+const tipPerPersonEl = document.querySelector(
+  ".tip-wrapper__tip-amount-per-person"
+);
+const totalPerPersonEl = document.querySelector(
+  ".tip-wrapper__total-amount-per-person"
+);
+
+//////////// CALC LOGIC ////////////
+function runMath() {
+  if (billAmount && tipPercent && numberOfPeople) {
+    tipDecimal = tipPercent / 100 + 1;
+    totalAmount = billAmount * tipDecimal;
+    tipAmount = tipDecimal * billAmount - billAmount;
+    let tipPerPerson = tipAmount / numberOfPeople;
+    let totalPerPerson = totalAmount / numberOfPeople;
+
+    tipPerPersonEl.innerText = tipPerPerson.toFixed();
+    totalPerPersonEl.innerText = totalPerPerson.toFixed();
+  }
+}
+
+// Form validation, error messages
+// (error: can't be 0 --> show red border + message)
+// Tests?
